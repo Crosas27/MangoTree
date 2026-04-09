c02a975bbed48173d85b00df6c5e9569b0ed7ef0
Crosas27 <c.rosas27@icloud.com>
2026-04-08 19:56:40 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'ui-element-forge-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './css/tokens.css',
+  './css/app.css',
+  './css/preview.css',
+  './js/utils.js',
+  './js/registry.js',
+  './js/schema-engine.js',
+  './js/style-dna-engine.js',
+  './js/constraint-engine.js',
+  './js/form-renderer.js',
+  './js/preview-engine.js',
+  './js/state-matrix.js',
+  './js/code-generator.js',
+  './js/storage.js',
+  './js/clipboard.js',
+  './js/app.js',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
